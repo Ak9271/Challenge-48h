da70318e412d85f2d99e56c6b44ad6b2fcc10258
nakchaya <akchayan.nihalananth@ynov.com>
2025-03-19 20:24:59 +0100

    correction de changement de pages et correction liaison css

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 	"log"
 	"net/http"
 	_ "modernc.org/sqlite"
-	"os"
 	"golang.org/x/crypto/bcrypt"
+	"html/template"
 )
 
 type InfoUser struct {
@@ -19,13 +19,14 @@ type InfoUser struct {
 
 var db *sql.DB
 
-func creerDB() {
-	database, err := sql.Open("sqlite", "./database.db")
+func creerDB() (*sql.DB, error) {
+	database, err := sql.Open("sqlite", "./test.db")
 	if err != nil {
 		return nil, err
 	}
 
-	creerDB := `CREATE TABLE IF NOT EXISTS users (
+	creerDB := 
+	`CREATE TABLE IF NOT EXISTS users (
 		"id" INTEGER PRIMARY KEY AUTOINCREMENT,
 		"nom" TEXT,
 		"email" TEXT,
@@ -46,9 +47,12 @@ func main() {
 	}
 	defer db.Close()
 
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/login", loginHandler)
-	http.HandleFunc("/signup", signupHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", renderTemplate("index.html"))
+	http.HandleFunc("/login", renderTemplate("login.html"))
+	http.HandleFunc("/signup", renderTemplate("signup.html"))
+	http.HandleFunc("/actualite", renderTemplate("actualite.html"))
+	http.HandleFunc("/contact", renderTemplate("contact.html"))
 	http.HandleFunc("/soumettre-signup", soumettreSignupHandler)
 	http.HandleFunc("/soumettre-login", soumettreLoginHandler)
 
@@ -56,79 +60,64 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/index.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+func renderTemplate(filename string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("template/" + filename)
+		if err != nil {
+			http.Error(w, "Erreur de chargement de la page", http.StatusInternalServerError)
+			return
+		}
+		tmpl.Execute(w, nil)
 	}
-	tmpl.Execute(w, nil)
-}
-
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/login.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	tmpl.Execute(w, nil)
-}
-
-func signupHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/signup.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	tmpl.Execute(w, nil)
 }
 
 func soumettreSignupHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		r.ParseForm()
-		nom := r.FormValue("nom")
-		email := r.FormValue("email")
-		mdp := r.FormValue("mdp")
-		hash, err := bcrypt.GenerateFromPassword([]byte(mdp), bcrypt.DefaultCost)
-		if err != nil {
-			http.Error(w, "Erreur lors du hash du mot de passe", http.StatusInternalServerError)
-			return
-		}
-
-		_, err = db.Exec("INSERT INTO users (nom, email, mdp) VALUES (?, ?, ?)", nom, email, hash)
-		if err != nil {
-			http.Error(w, "Erreur lors de l'inscription", http.StatusInternalServerError)
-			return
-		}
-
-		http.Redirect(w, r, "/login", http.StatusFound)
-	} else {
 		http.Error(w, "Méthode non autorisée", http.StatusMethodNotAllowed)
+		return
 	}
+
+	r.ParseForm()
+	nom := r.FormValue("nom")
+	email := r.FormValue("email")
+	mdp := r.FormValue("mdp")
+	hash, err := bcrypt.GenerateFromPassword([]byte(mdp), bcrypt.DefaultCost)
+
+	if err != nil {
+		http.Error(w, "Erreur lors du hash du mot de passe", http.StatusInternalServerError)
+		return
+	}
+
+	_, err = db.Exec("INSERT INTO users (nom, email, mdp) VALUES (?, ?, ?)", nom, email, hash)
+	if err != nil {
+		http.Error(w, "Erreur lors de l'inscription", http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/login", http.StatusFound)
 }
 
 func soumettreLoginHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-		r.ParseForm()
-		email := r.FormValue("email")
-		mdp := r.FormValue("mdp")
-
-		var user InfoUser
-		row := db.QueryRow("SELECT id, nom, email, mdp FROM users WHERE email = ?", email).Scan(&user.ID, &user.Nom, &user.Email, &user.Mdp)
-		err := row.Scan(&user.ID, &user.Nom, &user.Email, &user.Mdp)
-		if err != nil {
-			http.Error(w, "Utilisateur introuvable", http.StatusUnauthorized)
-			return
-		}
-		err = bcrypt.CompareHashAndPassword([]byte(user.Mdp), []byte(mdp))
-		if err != nil {
-			http.Error(w, "Mot de passe incorrect", http.StatusUnauthorized)
-			return
-		}
-		fmt.Fprintf(w, "Bienvenue %s", user.Nom)
-
-	} else {
 		http.Error(w, "Méthode non autorisée", http.StatusMethodNotAllowed)
+		return
 	}
-}
 
+	r.ParseForm()
+	email := r.FormValue("email")
+	mdp := r.FormValue("mdp")
+
+	var user InfoUser
+	err := db.QueryRow("SELECT id, nom, email, mdp FROM users WHERE email = ?", email).Scan(&user.ID, &user.Nom, &user.Email, &user.Mdp)
+
+	if err != nil {
+		http.Error(w, "Utilisateur introuvable", http.StatusUnauthorized)
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Mdp), []byte(mdp))
+	if err != nil {
+		http.Error(w, "Mot de passe incorrect", http.StatusUnauthorized)
+		return
+	}
+	fmt.Fprintf(w, "Bienvenue %s", user.Nom)
+}
--- static/js/script.js
+++ static/js/script.js
@@ -1,63 +1,29 @@
-const FormumlaireConnexion = document.getElementById('formumlaire-connexion');
-const FormumlaireInscription = document.getElementById('formumlaire-inscription');
-const MsgConnexion = document.getElementById('msg-connexion');
-const MsgInscription = document.getElementById('msg-inscription');
-
-loginForm.addEventListener('submit', function(e) {
-    e.preventDefault(); //Empêche rafraichissement de la page pendant la soumission  mail et mdp
-
-    const email = document.getElementById('email-connexion').value;
-    const mdp = document.getElementById('mdp-connexion').value;
-    const infos = new URLSearchParams();
-    infos.append('email', email);
-    infos.append('mdp', mdp);
-
-    fetch ('/connexion', {
-        method: 'POST',
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded', //Envoi des données en format URL
-        },
-    })
-    .then(response => response.text())
-    .then(data => {
-        if (data ==='Connexion réussie') {
-        MsgConnexion.textContent = 'Connexion réussie';
-        } else {
-            MsgConnexion.textContent = 'Identifiants Incorrects';
-        }
-    })
-    .catch(error =>{
-        MsgConnexion.textContent = 'Erreur de connexion';
-    });
-});
-
-registerForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    const nom = document.getElementById('nom-inscription').value;
-    const email = document.getElementById('email-inscription').value;
-    const mdp = document.getElementById('mdp-inscription').value;
-    const infos = new URLSearchParams();
-    infos.append('nom', nom);
-    infos.append('email', email);
-    infos.append('mdp', mdp);
-
-    fetch('/inscription', {
-        method: 'POST',
-        header: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: infos,
-    })
-    .then(response => response.text())
-    .then(data => {
-        if (data === 'Inscription réussie') {
-            MsgInscription.textContent = 'Inscription réussie';
-        } else {
-            MsgInscription.textContent = 'Erreur lors de l\'inscription';
-        }
-    })
-    .catch(error => {
-        MsgInscription.textContent = 'Erreur de l\'inscription';
-    });
-});
+// Formulaire de contact
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message envoyé!');
+  });
+  
+  // Inscriptions aux événements
+  document.getElementById('registerBtn').addEventListener('click', function() {
+    alert('Inscription réussie!');
+  });
+  
+  // Connexion
+  document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Bienvenue, vous êtes connecté!');
+  });
+  
+  // Inscription
+  document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Inscription réussie!');
+  });
+  
+  // Administrateur - Mise à jour des événements
+  document.getElementById('updateForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Activité mise à jour!');
+  });
+  
