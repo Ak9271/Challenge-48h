3081c48eaf23aac686fb1a38cddd6c5473dd9444
nakchaya <akchayan.nihalananth@ynov.com>
2025-03-19 22:22:54 +0100

    ajout de la page de formulaire de demande

--- static/js/script.js
+++ static/js/script.js
@@ -1,29 +1,23 @@
-// Formulaire de contact
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Message envoyé!');
-  });
-  
-  // Inscriptions aux événements
-  document.getElementById('registerBtn').addEventListener('click', function() {
+});
+
+document.getElementById('registerBtn').addEventListener('click', function() {
     alert('Inscription réussie!');
-  });
-  
-  // Connexion
-  document.getElementById('loginForm').addEventListener('submit', function(event) {
+});
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Bienvenue, vous êtes connecté!');
-  });
-  
-  // Inscription
-  document.getElementById('signupForm').addEventListener('submit', function(event) {
+});
+
+document.getElementById('signupForm').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Inscription réussie!');
-  });
-  
-  // Administrateur - Mise à jour des événements
-  document.getElementById('updateForm').addEventListener('submit', function(event) {
+});
+
+document.getElementById('updateForm').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Activité mise à jour!');
-  });
-  
+});
